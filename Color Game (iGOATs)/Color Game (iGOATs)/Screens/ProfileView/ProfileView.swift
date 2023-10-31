d23d59b3800f67db426c42f003c4a0c6736c06d2
Dias <bekpayev.dias@gmail.com>
2023-10-31 11:29:52 +0600

    added choosing photo option

--- Color Game (iGOATs)/Color Game (iGOATs)/Screens/ProfileView/ProfileView.swift
+++ Color Game (iGOATs)/Color Game (iGOATs)/Screens/ProfileView/ProfileView.swift
@@ -6,19 +6,49 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ProfileView: View {
     @ObservedObject var viewModel: ProfileViewModel
+    @State var selectedImage: UIImage?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
     
     var body: some View {
         LazyVStack {
-            Image("person.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 150, height: 150)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(Color.blue, lineWidth: 5))
-                .padding(.top, 20)
+            
+            if let image = selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.blue, lineWidth: 5))
+                    .padding(.top, 20)
+            } else {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.blue, lineWidth: 5))
+                    .padding(.top, 20)
+            }
+            
+            Button("Выбрать фото") {
+                let imagePicker = UIImagePickerController()
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.delegate = self.makeCoordinator()
+                if let windowScene = UIApplication.shared.connectedScenes
+                    .filter({ $0.activationState == .foregroundActive })
+                    .first as? UIWindowScene {
+                    if let window = windowScene.windows.first {
+                        window.rootViewController?.present(imagePicker, animated: true, completion: nil)
+                    }
+                }
+            }
             
             Text("Имя Игрока")
                 .font(.title)
@@ -45,6 +75,23 @@ struct ProfileView: View {
         }
     }
 }
+
+class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var parent: ProfileView
+    
+    init(_ parent: ProfileView) {
+        self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            parent.selectedImage = selectedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
 
 //struct Profile_Previews: PreviewProvider {
 //    static var previews: some View {

ad189aeb09e014db5f9a8f304002d3cd9712cb4f
qalamqas <kalamkas@mail.ru>
2023-11-05 20:57:25 +0600

    changes in logic: added shapes count depends on difficalty level

--- Color Game (iGOATs)/Color Game (iGOATs)/Screens/GameView/GameViewModel.swift
+++ Color Game (iGOATs)/Color Game (iGOATs)/Screens/GameView/GameViewModel.swift
@@ -24,11 +24,18 @@ class GameViewModel: ObservableObject {
     @Published var ifCorrectAnswer = 0
     @Published var pressedButtonIndex: Int?
     @Published var correctColor: Color?
+    @Published var columns = Array(repeating: GridItem(.fixed(60)), count: 4)
     var temp: Set<Color> = []
     var temp2: Set<Color> = []
     var tempArray: [Color] = []
     var tempArray2: [Color] = []
     
+    @Published var shapeCount = 0 {
+        didSet {
+            print("shapeCount = \(shapeCount)")
+        }
+    }
+
     init(mode: Mode, difficulty: Difficulty, blindnessType: BlindnessType, router: Router) {
         self.router = router
         self.mode = mode
@@ -53,6 +60,20 @@ class GameViewModel: ObservableObject {
     }
     
     func startRound() {
+        switch self.difficulty {
+        case .babyTime1: shapeCount = 4; columns = Array(repeating: GridItem(.fixed(60)), count: 2)
+        case .babyTime2: shapeCount = 9; columns = Array(repeating: GridItem(.fixed(60)), count: 3)
+        case .easy1: shapeCount = 16; columns = Array(repeating: GridItem(.fixed(60)), count: 4)
+        case .easy2: shapeCount = 20; columns = Array(repeating: GridItem(.fixed(60)), count: 4)
+        case .easy3: shapeCount = 24; columns = Array(repeating: GridItem(.fixed(60)), count: 4)
+        case .medium1: shapeCount = 16; columns = Array(repeating: GridItem(.fixed(60)), count: 4)
+        case .medium2: shapeCount = 20; columns = Array(repeating: GridItem(.fixed(60)), count: 5)
+        case .medium3: shapeCount = 24; columns = Array(repeating: GridItem(.fixed(60)), count: 6)
+        case .hard1: shapeCount = 16; columns = Array(repeating: GridItem(.fixed(60)), count: 4)
+        case .hard2: shapeCount = 20; columns = Array(repeating: GridItem(.fixed(60)), count: 5)
+        case .hard3: shapeCount = 24; columns = Array(repeating: GridItem(.fixed(60)), count: 4)
+        }
+        
         self.ifCorrectAnswer = 0
         if self.mode == .colorBlindTest {
             colors = colorBlind(blindnessType: blindnessType)
@@ -63,7 +84,7 @@ class GameViewModel: ObservableObject {
     
     func proceedUserInput(_ index: Int) {
         if isCorrect(index) {
-            if difficulty != .hard {difficulty = difficulty.next()}
+            if difficulty != .hard3 {difficulty = difficulty.next()}
             AudioServicesPlaySystemSound(1109)
             withAnimation(.bouncy(duration: 0.2, extraBounce: 0.2)) {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
@@ -80,28 +101,38 @@ class GameViewModel: ObservableObject {
         var set2: Set<Color> = []
         var set3: Set<Color> = []
         var set4: Set<Color> = []
-//        var tempArray: [Color] = []
+        let result: [Color]
         
-        while set1.count < 4 {
+        while set1.count < 6 {
             set1.insert(Color.randomYellow())
         }
-        
-        while set2.count < 4 {
+        while set2.count < 6 {
             set2.insert(Color.randomRed())
         }
-        
-        while set3.count < 4 {
+        while set3.count < 6 {
             set3.insert(Color.randomBlue())
         }
-        
-        while set4.count < 4 {
+        while set4.count < 6 {
             set4.insert(Color.randomGreen())
         }
 
         tempArray = Array(set1.union(set2).union(set3).union(set4))
-        tempArray[14] = tempArray[15]
-        tempArray.shuffle()
-        return tempArray
+        
+        switch difficulty {
+        case .babyTime1: result = babyTime1(colors: tempArray)
+        case .babyTime2: result = babyTime2(colors: tempArray)
+        case .easy1: result = easy1(colors: tempArray)
+        case .easy2: result = easy2(colors: tempArray)
+        case .easy3: result = easy3(colors: tempArray)
+        case .medium1: result = medium1(colors: tempArray)
+        case .medium2: result = medium2(colors: tempArray)
+        case .medium3: result = medium3(colors: tempArray)
+        case .hard1: result = hard1(colors: tempArray)
+        case .hard2: result = hard2(colors: tempArray)
+        case .hard3: result = hard3(colors: tempArray)
+        }
+        
+        return result
     }
     
     func colorBlind(blindnessType: BlindnessType) -> [Color] {
@@ -162,4 +193,93 @@ class GameViewModel: ObservableObject {
             }
         }
     }
+    
+    func babyTime1(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(4))
+        temp.shuffle()
+        temp[2] = temp[3]
+        temp.shuffle()
+        return temp
+    }
+    
+    func babyTime2(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(9))
+        temp.shuffle()
+        temp[7] = temp[8]
+        temp.shuffle()
+        return temp
+    }
+    
+    func easy1(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(16))
+        temp.shuffle()
+        temp[14] = temp[15]
+        temp.shuffle()
+        return temp
+    }
+    
+    func easy2(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(20))
+        temp.shuffle()
+        temp[19] = temp[18]
+        temp.shuffle()
+        return temp
+    }
+    
+    func easy3(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(24))
+        temp.shuffle()
+        temp[23] = temp[22]
+        temp.shuffle()
+        return temp
+    }
+    
+    func medium1(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(16))
+        temp.shuffle()
+        temp[14] = temp[15]
+        temp.shuffle()
+        return temp
+    }
+    
+    func medium2(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(20))
+        temp.shuffle()
+        temp[19] = temp[18]
+        temp.shuffle()
+        return temp
+    }
+    
+    func medium3(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(24))
+        temp.shuffle()
+        temp[23] = temp[22]
+        temp.shuffle()
+        return temp
+    }
+    
+    func hard1(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(16))
+        temp.shuffle()
+        temp[14] = temp[15]
+        temp.shuffle()
+        return temp
+    }
+    
+    func hard2(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(20))
+        temp.shuffle()
+        temp[19] = temp[18]
+        temp.shuffle()
+        return temp
+    }
+    
+    func hard3(colors: [Color]) -> [Color] {
+        var temp = Array(colors.prefix(24))
+        temp.shuffle()
+        temp[23] = temp[22]
+        temp.shuffle()
+        return temp
+    }
+
 }

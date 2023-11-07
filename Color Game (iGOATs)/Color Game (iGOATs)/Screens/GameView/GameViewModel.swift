8d8d02d715d5c76adfcc12ad5a6d6461f5ee3ec5
qalamqas <kalamkas@mail.ru>
2023-11-07 14:38:19 +0600

    minor changes

--- Color Game (iGOATs)/Color Game (iGOATs)/Screens/GameView/GameViewModel.swift
+++ Color Game (iGOATs)/Color Game (iGOATs)/Screens/GameView/GameViewModel.swift
@@ -40,6 +40,9 @@ final class GameViewModel: ObservableObject {
     @Published var shapeCount = 0
     @Published var colorblindText = ""
     @Published var colorblindTextColor = Color.cyan
+    @Published var isRightPallete = false
+    @Published var showingBlindnessAlert = false
+    @Published var alertText = ""
     
     var temp: Set<Color> = []
     var temp2: Set<Color> = []
